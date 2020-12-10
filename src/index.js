64da1247de4ad5a811f6b30b5a824a00831243b0
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2020-12-10 10:27:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const { newsArticleModel } = require('./connector');
 
 const onePageArticleCount = 10
 
@@ -9,7 +10,14 @@ const onePageArticleCount = 10
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get('/newFeeds', (req, res) => {
+    let limit = isNaN(req.query.limit) || !req.query.limit ? 10 : parseInt(req.query.limit);
+    let offset = isNaN(req.query.offset) || !req.query.offset ? 0 : parseInt(req.query.offset);
 
+    newsArticleModel.find().limit(limit).skip(offset).then((result) => {
+        res.send(result);
+    });
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
